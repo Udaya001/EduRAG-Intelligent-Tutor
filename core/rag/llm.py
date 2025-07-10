2eef5b83be513a3a12ac16b1609efbeb0bb0ced3
iamudaya <usharma4501@gamil.com>
2025-07-11 00:31:07 +0545

    fixed import issue

--- core/rag/llm.py
+++ core/rag/llm.py
@@ -1,9 +1,10 @@
-from langchain_google_genai import ChatGoogleGenerativeAI
-from core.config import settings
+from langchain_google_vertexai import ChatVertexAI
 
-def get_llm(model_name="models/gemini-1.5-flash"):
-    return ChatGoogleGenerativeAI(
-        model=model_name,
-        google_api_key=settings.GOOGLE_API_KEY,
-        temperature=0.3
+def get_llm():
+    return ChatVertexAI(
+        model_name="gemini-2.5-flash",
+        temperature=0.3,
+        location="us-central1",
     )
+
+
